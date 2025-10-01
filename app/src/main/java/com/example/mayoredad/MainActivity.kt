e1960547ceb2497e29fd0fb3ec3f3a2db5d77872
Chad <chad154154@gmail.com>
2025-10-01 13:20:59 +0200

    Realizado la entrada de datos tanto de edad como la validacion de estos con diferentes resultado, ademas hay dos formas diferentes de hacerlo

--- app/src/main/java/com/example/mayoredad/MainActivity.kt
+++ app/src/main/java/com/example/mayoredad/MainActivity.kt
@@ -9,6 +9,9 @@ import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import com.example.mayoredad.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -22,38 +25,26 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        setSupportActionBar(binding.toolbar)
+        val numero: EditText = findViewById(R.id.editText1)
+        val boton: Button = findViewById(R.id.button1)
+        val resultado: TextView = findViewById(R.id.textView1)
 
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
-        appBarConfiguration = AppBarConfiguration(navController.graph)
-        setupActionBarWithNavController(navController, appBarConfiguration)
-
-        binding.fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null)
-                .setAnchorView(R.id.fab).show()
+        boton.setOnClickListener {
+            //Recoger el valor del campo texto, pasarlo a entero o null, meter in if dependiendo del valor da un resultado distinguiendo el nulo
+            val edadString = numero.text.toString().toIntOrNull()
+            /*
+            if (edadString == null)
+                resultado.text = "Introduzca un valor"
+            else if (edadString < 18)
+                resultado.text = "Eres menor de edad"
+            else if (edadString > 18)
+                resultado.text = "Eres mayor de edad"
+            else resultado.text = "Tienes justo 18 años" */
+            resultado.text = if (edadString == null) "Introduzca un valor"
+            else if (edadString < 18) "Eres menor de edad"
+            else if (edadString > 18) "Eres mayor de edad"
+            else "Tienes justo 18 años"
         }
-    }
 
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        return when (item.itemId) {
-            R.id.action_settings -> true
-            else -> super.onOptionsItemSelected(item)
-        }
-    }
-
-    override fun onSupportNavigateUp(): Boolean {
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
-        return navController.navigateUp(appBarConfiguration)
-                || super.onSupportNavigateUp()
     }
 }
